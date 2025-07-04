493e16f5fb83f103023b11b63f42d2ae677ca877
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-07-05 04:00:39 +0530

    navbar change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
     return (
-        <div className="flex items-center w-[90vw] max-w-[1260px] justify-between mx-auto bg-[#1a1a1a] p-5 mt-5 rounded-4xl">
+        <div className="flex items-center w-[94vw] max-w-[1260px] justify-between mx-auto bg-[#1a1a1a] py-5 px-10 mt-5 rounded-4xl">
             <div>
-                <h1 className="text-lg sm:text-2xl text-white font-semibold">Zaldera</h1>
+                <h1 className="text-lg sm:text-3xl text-white tracking-widest font-semibold">ZALDERA</h1>
             </div>
             <div className="md:flex hidden items-center gap-10 text-white/60 md:text-xs lg:text-[15px]">
                 <p className="cursor-pointer hover:text-white duration-300">Home</p>
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <p className="cursor-pointer hover:text-white duration-300">Contact Us</p>
             </div>
             <div className="text-white block md:hidden"><IoMenu className="size-6" /></div>
-            <button className="py-2 px-6 text-lg  rounded-4xl cursor-pointer hover:bg-gray-500/70 hover:text-white duration-300 text-white/80 bg-gray-500/40">Free Trial</button>
+            <button className="py-2 px-6 text-lg mona_sans rounded-4xl Mona-Sans cursor-pointer hover:bg-gray-500/70 hover:text-white duration-300 text-white/80 bg-gray-500/40">Free Trial</button>
         </div>
     )
 }
